0e48395ba0ae088215b28f94b375fae34e86024a
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-11-29 17:08:19 +0530

    Create solution.cs

---
+++ solution.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        Dictionary<int, int> d = new Dictionary<int, int>();
+        for(int i = 0; i < nums.Length; i++){
+            if(d.ContainsKey(nums[i])) return true;
+            else d[nums[i]] = 1;
+        }
+
+        return false;
+    }
+}
